f1bc8f16269bc51545cdaf36e0bc29161a352fdb
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-11-14 21:12:03 +0100

    markup navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,84 @@
 import { NavLink } from "react-router-dom";
+import "../scss/navigation.scss";
 
 export default function Navigation() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/">Project</NavLink>
-      <NavLink to="/">About me</NavLink>
-      <NavLink to="/">Contact</NavLink>
-      <NavLink to="/">Fedback</NavLink>
+    <nav class="navigation">
+      <NavLink className="navigation__nav" to="/">
+        <svg width={200} height={60}>
+          <image className="img" href="../../public/subtract.svg" x="0" y="0" />
+
+          <text
+            x="80"
+            y="18"
+            // font-size="18"
+            fill="white"
+            // text-anchor="middle"
+          >
+            Home
+          </text>
+        </svg>
+      </NavLink>
+      <NavLink className="navigation__nav" to="/">
+        <svg width={200} height={40}>
+          <image className="img" href="../../public/subtract.svg" x="0" y="0" />
+
+          <text
+            x="80"
+            y="18"
+            // font-size="18"
+            fill="white"
+            // text-anchor="middle"
+          >
+            Project
+          </text>
+        </svg>
+      </NavLink>
+      <NavLink className="navigation__nav" to="/">
+        <svg width={200} height={40}>
+          <image className="img" href="../../public/subtract.svg" x="0" y="0" />
+
+          <text
+            x="80"
+            y="18"
+            // font-size="18"
+            fill="white"
+            // text-anchor="middle"
+          >
+            About me
+          </text>
+        </svg>
+      </NavLink>
+      <NavLink className="navigation__nav" to="/">
+        <svg width={200} height={40}>
+          <image className="img" href="../../public/subtract.svg" x="0" y="0" />
+
+          <text
+            x="80"
+            y="18"
+            // font-size="18"
+            fill="white"
+            // text-anchor="middle"
+          >
+            Contact
+          </text>
+        </svg>
+      </NavLink>
+      <NavLink className="navigation__nav" to="/">
+        <svg width={200} height={40}>
+          <image className="img" href="../../public/subtract.svg" x="0" y="0" />
+
+          <text
+            x="80"
+            y="18"
+            // font-size="18"
+            fill="white"
+            // text-anchor="middle"
+          >
+            Fedback
+          </text>
+        </svg>
+      </NavLink>
     </nav>
   );
 }
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,17 +1,18 @@
 import HeroImg from "../components/HeroImg";
 import LeftBar from "../components/LeftBar";
+import Navigation from "../components/Navigation";
 import RightBar from "../components/RightBar";
 import "../scss/containerHomePage.scss";
 
 export default function () {
   return (
     <>
-      <p>HOME PAGE</p>
       <div className="container">
         <LeftBar />
         <HeroImg />
         <RightBar />
       </div>
+      <Navigation />
     </>
   );
 }
