3e0652063b9078ffe253d0a981828f4f01de1555
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-11-17 23:09:02 +0100

    project page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,35 @@
+// коммандный проект Team project
+// Сделано лично Made personally Self-performed
+
 export default function Project() {
   return (
     <>
       <p>Project</p>
+      <section>
+        <div>
+          <img src="" alt="" />
+          <p>Individual project</p>
+        </div>
+        <div>
+          <h2> WebStudio</h2>
+          <p>
+            The site is designed with a semantic structure, adaptive layout, and
+            an intuitive interface.
+          </p>
+          <ul>
+            <li>
+              <svg
+                // class="advantages-icon"
+                // aria-label="стратегия"
+                width="32"
+                height="32"
+              >
+                <use href=""></use>
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
