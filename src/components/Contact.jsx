cdf9ba3e3de2c816fb45ac1464a8dd5c760c4b8a
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-12-25 01:17:00 +0100

    styles contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,94 +3,102 @@ import "../scss/contact.scss";
 export default function Contact() {
   return (
     <>
-      <ul className="contact-list">
-        <li className="contact-list__item">
-          <svg
-            className="contact-list__contact-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-phone"></use>
-          </svg>
-          <p>+38011-111-11-11 </p>
-        </li>
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-email"></use>
-          </svg>
-          <p>qwerty@qwe.rty </p>
-        </li>
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-telegram"></use>
-          </svg>
-          <p>telegram </p>
-        </li>
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            aria-label="whatsapp"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-whatsapp"></use>
-          </svg>
-          <p>whatsapp </p>
-        </li>
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-linkedin"></use>
-          </svg>
-          <p>link </p>
-        </li>
+      <section className="contacts">
+        <ul className="contact-list">
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-phone"></use>
+            </svg>
+            <p className=" contact-list__contact">+38011-111-11-11 </p>
+          </li>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-email"></use>
+            </svg>
+            <p className=" contact-list__contact">qwerty@qwe.rty </p>
+          </li>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-telegram"></use>
+            </svg>
+            <p className=" contact-list__contact">telegram </p>
+          </li>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              aria-label="whatsapp"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-whatsapp"></use>
+            </svg>
+            <p className=" contact-list__contact">whatsapp </p>
+          </li>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-linkedin"></use>
+            </svg>
+            <p className=" contact-list__contact">linkedin </p>
+          </li>
 
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-github"></use>
-          </svg>
-          <p>github </p>
-        </li>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-github"></use>
+            </svg>
+            <p className=" contact-list__contact">github </p>
+          </li>
 
-        <li className="contact-list__item">
-          <svg
-            // className="card-container__project-icon"
-            // class="advantages-icon"
-            // aria-label="стратегия"
-            width="32"
-            height="32"
-          >
-            <use href="../../public/contact/contact_set.svg#icon-location"></use>
-          </svg>
-          <p>city </p>
-        </li>
-      </ul>
+          <li className="contact-list__item">
+            <svg
+              className="contact-list__contact-icon"
+              // className="card-container__project-icon"
+              // class="advantages-icon"
+              // aria-label="стратегия"
+              width="32"
+              height="32"
+            >
+              <use href="../../public/contact/contact_set.svg#icon-location"></use>
+            </svg>
+            <p className=" contact-list__contact">city </p>
+          </li>
+        </ul>
+      </section>
     </>
   );
 }
